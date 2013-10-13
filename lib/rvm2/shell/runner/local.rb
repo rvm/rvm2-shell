e8e7dfcbac69110bb3e2861599a26b98c7e36ae8
Michal Papis <mpapis@gmail.com>
2013-10-13 22:56:48 +0200

    add :last_status for local runner

--- lib/rvm2/shell/runner/local.rb
+++ lib/rvm2/shell/runner/local.rb
@@ -1,16 +1,20 @@
 require 'session'
 
 class Rvm2::Shell::Runner::Local
-  attr_reader :shell
+  attr_reader :shell, :last_status
+
   def initialize(shell = "sh")
     @shell = shell
   end
+
   def execute(command)
+    @last_status = nil
     shell_session.execute(command) do |out, err|
       yield(out, err)
     end
-    shell_session.status
+    @last_status = shell_session.status
   end
+
   def shell_session
     @shell_session ||= Session::Sh.new(:prog => shell)
   end
